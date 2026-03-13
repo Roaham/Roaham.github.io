f403d013300b5d2dffd8c09198ea0bd8de4d1f85
Roaham <roripro2007@gmail.com>
2026-03-13 13:39:31 +0100

    #	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script>
-  import "./app.css";
+  import "../app.css";
+  
+  // En Svelte 5, los componentes usan $props()
+  let { children } = $props();
 </script>
 
 <div class="main-content">
   <main class="content">
-    <slot />
+    {#if children}
+      {@render children()}
+    {/if}
   </main>
 </div>
